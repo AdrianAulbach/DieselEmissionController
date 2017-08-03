f3e9857ad204f387437026db3098657914917ee6
Adrian Aulbach <adrian@aulbach.ch>
2017-08-03 19:32:56 +0200

    Shit! We got busted. At the Diesel-Gipfel we had to agree to change the software. The politicians are stupid enough to believe us that this will solve the problem. (Ok, we MAY have paid them to be a little stupid whenever it suits us ...) Let's hope CDU and especially Dobrindt stay in power. Otherwise we might be forced to find a real solution :(

--- EmissionController.java
+++ EmissionController.java
@@ -8,13 +8,13 @@ public class EmissionController {
     public void setEmissions() {
         Motor motor = car.getMotor();
         
+        motor.setNOx(170); //set NOx emissions to 170mg/km
+        
         if(CheatDevice.testCycleDetected()) {
             //Car seems to be passing an emission test. Let's play nice for once.
-            motor.setNOx(170); //set NOx emissions to 170mg/km
             motor.setCO2(130); //set CO2 emissions to 130g/km
             motor.setNoise(75); //set noise to 75dB. Sooo boring. Total disaster.
         } else {
-            motor.setNOx(2400); //set NOx emissions to 2400mg/km and people still think it's smoking that gives them cancer. LOL.
             motor.setCO2(190); //set CO2 emissions to 190g/km. Look at me. Do I look like a Prius?
             motor.setNoise(90); //set noise to 90dB, our customers want to wake up their neighbours, otherwise they would ride a bike.
         }
